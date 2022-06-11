69d4ee0e34057716dbc2ff281a900c5b0cbf7be4
Isaiah A <isaiah.althouse@gmail.com>
2022-06-10 22:27:20 -0700

    Add standard global middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,30 @@
 import express, { Request, Response } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import compression from 'compression';
 import { env } from './config/env'
 
 const app = express();
+
+// TODO: Logging
+// TODO: Sanitization?
+// TODO: Error catching
+
+
+// Sets security HTTP Headers
+app.use(helmet());
+
+// Parse JSON RequestBody
+app.use(express.json());
+
+// Parse urlencoded RequestBody
+app.use(express.urlencoded({ extended: true }));
+
+// Use gzip compression
+app.use(compression());
+
+// Enable CORS
+app.use(cors({}));
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
