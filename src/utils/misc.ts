c4ac6e34db9e822f46c3330365c48179ba1151a7
Isaiah A <isaiah.althouse@gmail.com>
2022-06-11 13:38:48 -0700

    Remove tmp files verifying Jest setup

--- src/utils/misc.ts
+++
@@ -1,3 +0,0 @@
-export function randomFn() {
-  return 2;
-}
--- tests/utils/misc.test.ts
+++
@@ -1,7 +0,0 @@
-import { randomFn } from '../../src/utils/misc';
-
-describe('Confirm tests run correctly', () => {
-  test('test 1', () => {
-    expect(randomFn()).toBe(2);
-  });
-});
