25c321ba58ee6e1e7439c0f79f5694550446af7a
Isaiah A <isaiah.althouse@gmail.com>
2022-06-12 19:22:30 -0700

    Add stub UserService and GET/users endpoint

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,11 @@
 import { model, Schema } from 'mongoose';
 
-const userSchema = new Schema({
+interface IUser {
+  username: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>({
   username: {
     type: String, required: true, minLength: 3, maxLength: 15,
   },
